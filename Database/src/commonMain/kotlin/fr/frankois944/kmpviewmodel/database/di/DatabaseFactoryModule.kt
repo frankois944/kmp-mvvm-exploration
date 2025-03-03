d9346008f8f36b57ca099b6dc72d31744d966af6
frankois <dabonot.francois@gmail.com>
2025-03-03 19:03:14 +0100

    better Koin annotation

--- Database/src/commonMain/kotlin/fr/frankois944/kmpviewmodel/database/di/DatabaseFactoryModule.kt
+++ Database/src/commonMain/kotlin/fr/frankois944/kmpviewmodel/database/di/DatabaseFactoryModule.kt
@@ -3,17 +3,20 @@ package fr.frankois944.kmpviewmodel.database.di
 import fr.frankois944.kmpviewmodel.database.factory.DBFactory
 import fr.frankois944.kmpviewmodel.database.room.AppDatabase
 import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Factory
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Provided
 import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("fr.frankois944.kmpviewmodel.database.room")
 public class DatabaseFactoryModule {
-    internal val dbFactory: DBFactory = DBFactory()
-
-    // @Factory
-    // internal fun dbFactory(): DBFactory = DBFactory()
+    @Factory
+    internal fun dbFactory(): DBFactory = DBFactory()
 
     @Single
-    internal fun database(): AppDatabase = dbFactory.createRoomDatabase()
+    internal fun database(
+        @Provided
+        factory: DBFactory,
+    ): AppDatabase = factory.createRoomDatabase()
 }
