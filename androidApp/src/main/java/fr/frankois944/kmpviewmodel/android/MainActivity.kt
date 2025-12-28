f49db93221a7fc09acd38963c585c736c9798cef
frankois <dabonot.francois@gmail.com>
2025-12-28 12:14:43 +0100

    update deps
    migration of compose

--- androidApp/src/main/java/fr/frankois944/kmpviewmodel/android/MainActivity.kt
+++ androidApp/src/main/java/fr/frankois944/kmpviewmodel/android/MainActivity.kt
@@ -1,4 +1,4 @@
-@file:OptIn(KoinExperimentalAPI::class, ExperimentalMaterial3Api::class)
+@file:OptIn(KoinExperimentalAPI::class, ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 
 package fr.frankois944.kmpviewmodel.android
 
@@ -6,12 +6,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomAppBarState
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -80,7 +94,7 @@ class MainActivity : ComponentActivity() {
                                         navController.navigateUp()
                                     }) {
                                         Icon(
-                                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                            painter = painterResource(R.drawable.arrow_back),
                                             contentDescription = "Localized description",
                                         )
                                     }
